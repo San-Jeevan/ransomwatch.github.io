f7e9e8bbfee0c76ddb8327bbceacade5fcbc9d4d
Jeevan Sivagnanasuntharam <sanjeevan@outlook.com>
2021-03-02 18:15:47 +0100

    fixed time

--- application.js
+++ application.js
@@ -4,6 +4,32 @@ Vue.component("modal", {
 });
 
 
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+
 function formatDate(date) {
   var d = new Date(date),
       month = '' + (d.getMonth() + 1),
@@ -24,17 +50,14 @@ var app4 = new Vue({
     this.loadSites()
   },
   data: {
-    showModal: false,
-    currentScreenshot : "",
+    selectedSite : {},
     ransomSites: [
-      { text: 'DopplePaymer', selected: true },
-      { text: 'REvil', selected: false },
-      { text: 'Conti', selected: false }
     ]
   },
   methods: {
-    getImgUrl(index) {
-      return this.currentScreenshot;
+    formatTime: function(index) {
+   
+      return timeSince(new Date(index));
   },
     changeSite: function (event) {
       var old = this.ransomSites.find(element => element.selected ===true);
@@ -42,8 +65,8 @@ var app4 = new Vue({
        old.selected = false;
      }
       event.selected = true;
-      this.currentScreenshot = `https://ransomwatchs3.s3.eu-north-1.amazonaws.com/screenshots/${event.siteid}/${formatDate(event.lastScreenshot)}.png`
-      
+      event.screenshot = `https://ransomwatchs3.s3.eu-north-1.amazonaws.com/screenshots/${event.siteid}/${formatDate(event.lastScreenshot)}.png`
+      this.selectedSite = event;
     },
     loadSites: function() {
       var self=this;
