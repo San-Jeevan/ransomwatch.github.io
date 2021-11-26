a6cfcd0868fd3c930f8946f8fd62e81a0e747b8e
Jeevan Sivagnanasuntharam <sanjeevan@outlook.com>
2021-11-26 16:23:24 +0100

    working submit links function

--- application.js
+++ application.js
@@ -84,32 +84,48 @@ var app4 = new Vue({
       this.selectedSite = event;
       scrollToImage();
     },
+    getSiteCrawlPreview: function (_siteUrl) {
+      console.log(`getSiteCrawlPreview called with input ${_siteUrl}`);
+      var self = this;
+      this.imageLoading = true;
+      axios.post('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomWatchPreviewSite', {
+        siteUrl: _siteUrl
+      }).then(function (response) {
+        self.imageLoading = false;
+        if (response.status == "200") {
+          console.log(response);
+          self.submitSiteUrlResponse = response.data.Message;
+        }
+      })
+    },
     submitSite: function (_siteUrl) {
       console.log(`submitSite called with input ${_siteUrl}`);
       var self = this;
       this.imageLoading = true;
-      this.$http.post('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomWatchSubmitNewSite', {
+      axios.post('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomWatchSubmitNewSite', {
         siteUrl: _siteUrl
       }).then(function (response) {
-        this.imageLoading = false;
+        self.imageLoading = false;
         if (response.status == "200") {
           console.log(response);
-          this.submitSiteUrlResponse = response.body.Message;
+          self.submitSiteUrlResponse = response.data.Message;
         }
       })
     },
     loadSites: function() {
       var self=this;
-      this.$http.get('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomScraperGetAllSites').then(function(response){
-      if(response.status == "200"){
-      self.ransomSites = response.body.filter(x=> x.lastScreenshot !== "").sort(function(a, b) {
-        var c = new Date(a.lastScreenshot);
-        var d = new Date(b.lastScreenshot);
-        return d-c;
-    });
-      this.changeSite(response.body[0])
-      }
-    })
+      axios.get('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomScraperGetAllSites')
+      .then(function (response) {
+        if(response.status == "200"){
+          self.ransomSites = response.data.filter(x=> x.lastScreenshot !== "").sort(function(a, b) {
+            var c = new Date(a.lastScreenshot);
+            var d = new Date(b.lastScreenshot);
+            return d-c;
+        });
+        self.changeSite(response.data[0])
+          }
+      });
+
   }
   }
 })
