ffb4f3cab183c10707ff7bdd5762fe749db355fd
Jeevan Sivagnanasuntharam <sanjeevan@outlook.com>
2021-11-26 17:58:08 +0100

    fully working crawl preview on submission

--- application.js
+++ application.js
@@ -27,19 +27,19 @@ function timeSince(date) {
 
 function formatDate(date) {
   var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
 
   return [day, month, year].join('.');
 }
 
-function scrollToImage(){
+function scrollToImage() {
   var ua = navigator.userAgent;
   var isMobile = /Android|webOS|iPhone|iPad|iPod/i.test(ua);
 
@@ -57,47 +57,52 @@ var app4 = new Vue({
   data: {
     imageLoading: false,
     view: 1,
-    selectedSite : {},
+    selectedSite: {},
     submitSiteUrl: "",
     submitSiteUrlResponse: "",
+    crawlPreviewSuccess: false,
+    crawlPreviewData: "",
     ransomSites: [
     ]
   },
-  methods: { 
+  methods: {
     ImgLoaded() {
-    console.log("img loaded");
-    this.imageLoading = false;
-},
-    formatTime: function(index) {
+      console.log("img loaded");
+      this.imageLoading = false;
+    },
+    formatTime: function (index) {
       return timeSince(new Date(index));
-  },
+    },
+    // --------CHANGESITE---------
     changeSite: function (event) {
-      this.view=1;
-      if(event.lastScreenshot == "") return;
-      var old = this.ransomSites.find(element => element.selected ===true);
-     if(old!= undefined) {
-       old.selected = false;
-     }
-     this.imageLoading = true;
+      this.view = 1;
+      if (event.lastScreenshot == "") return;
+      var old = this.ransomSites.find(element => element.selected === true);
+      if (old != undefined) {
+        old.selected = false;
+      }
+      this.imageLoading = true;
       event.selected = true;
       event.screenshot = `https://ransomwatchs3.s3.eu-north-1.amazonaws.com/screenshots/${event.siteid}/${formatDate(event.lastScreenshot)}.png`
       this.selectedSite = event;
       scrollToImage();
     },
+    // --------CrawlPREVIEW---------
     getSiteCrawlPreview: function (_siteUrl) {
       console.log(`getSiteCrawlPreview called with input ${_siteUrl}`);
       var self = this;
       this.imageLoading = true;
-      axios.post('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomWatchPreviewSite', {
+      axios.post('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomWatchScrape', {
         siteUrl: _siteUrl
       }).then(function (response) {
         self.imageLoading = false;
         if (response.status == "200") {
-          console.log(response);
-          self.submitSiteUrlResponse = response.data.Message;
+          self.crawlPreviewSuccess = response.data.success;
+          self.crawlPreviewData = response.data.data;
         }
       })
     },
+    // --------SUBMITSITE---------
     submitSite: function (_siteUrl) {
       console.log(`submitSite called with input ${_siteUrl}`);
       var self = this;
@@ -109,23 +114,26 @@ var app4 = new Vue({
         if (response.status == "200") {
           console.log(response);
           self.submitSiteUrlResponse = response.data.Message;
+          //if(response.data.Success) 
+          self.getSiteCrawlPreview(_siteUrl);
         }
       })
     },
-    loadSites: function() {
-      var self=this;
+    // --------LOADSITES---------
+    loadSites: function () {
+      var self = this;
       axios.get('https://qc1m8bddrd.execute-api.eu-north-1.amazonaws.com/Production/ransomScraperGetAllSites')
-      .then(function (response) {
-        if(response.status == "200"){
-          self.ransomSites = response.data.filter(x=> x.lastScreenshot !== "").sort(function(a, b) {
-            var c = new Date(a.lastScreenshot);
-            var d = new Date(b.lastScreenshot);
-            return d-c;
-        });
-        self.changeSite(response.data[0])
+        .then(function (response) {
+          if (response.status == "200") {
+            self.ransomSites = response.data.filter(x => x.lastScreenshot !== "").sort(function (a, b) {
+              var c = new Date(a.lastScreenshot);
+              var d = new Date(b.lastScreenshot);
+              return d - c;
+            });
+            self.changeSite(response.data[0])
           }
-      });
+        });
 
-  }
+    }
   }
 })
